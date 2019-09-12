466f7a7f134021c5fe1b84603e5353a5e1db7000
Terry <terry@khadas.com>
2019-09-12 09:28:11 +0800

    4G: fix 4G connect issue

--- libnetutils/ifc_utils.c
+++ libnetutils/ifc_utils.c
@@ -693,7 +693,14 @@ ifc_configure(const char *ifname,
 
     char dns_prop_name[PROPERTY_KEY_MAX];
 
+    if (strcmp(ifname, "usb0") == 0) {
+       snprintf(dns_prop_name, sizeof(dns_prop_name), "net.%s.dns1", ifname);
+       property_set(dns_prop_name, dns1 ? ipaddr_to_string(dns1) : "");
+       snprintf(dns_prop_name, sizeof(dns_prop_name), "net.%s.dns2", ifname);
+       property_set(dns_prop_name, dns2 ? ipaddr_to_string(dns2) : "");
+    }
     ifc_init();
+
 
     if (ifc_up(ifname)) {
         printerr("failed to turn on interface %s: %s\n", ifname, strerror(errno));
@@ -718,10 +725,11 @@ ifc_configure(const char *ifname,
 
     ifc_close();
 
-    snprintf(dns_prop_name, sizeof(dns_prop_name), "net.%s.dns1", ifname);
-    property_set(dns_prop_name, dns1 ? ipaddr_to_string(dns1) : "");
-    snprintf(dns_prop_name, sizeof(dns_prop_name), "net.%s.dns2", ifname);
-    property_set(dns_prop_name, dns2 ? ipaddr_to_string(dns2) : "");
-
+    if (strcmp(ifname, "usb0") != 0) {
+        snprintf(dns_prop_name, sizeof(dns_prop_name), "net.%s.dns1", ifname);
+        property_set(dns_prop_name, dns1 ? ipaddr_to_string(dns1) : "");
+        snprintf(dns_prop_name, sizeof(dns_prop_name), "net.%s.dns2", ifname);
+        property_set(dns_prop_name, dns2 ? ipaddr_to_string(dns2) : "");
+    }
     return 0;
 }
