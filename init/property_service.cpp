6f6462ed8d542113b5cb17391560cf04a1149a1d
Luke go <sangch.go@gmail.com>
2021-12-23 11:15:45 +0900

    ODROID: Add lcd_density property.
    - To support per display density feature, Add lcd_density property as
      kernel boot argument.
    
    Change-Id: Ibeb2aa36a7ef9a311591a44241e34b069bc2ed9e
    Signed-off-by: Luke go <sangch.go@gmail.com>
    
    Former-commit-id: 3d761506602e702885ab07a434c2199b7413ab5c

--- init/property_service.cpp
+++ init/property_service.cpp
@@ -1229,6 +1229,7 @@ static void ExportKernelBootProps() {
         { "ro.boot.baseband",   "ro.baseband",   "unknown", },
         { "ro.boot.bootloader", "ro.bootloader", "unknown", },
         { "ro.boot.hardware",   "ro.hardware",   "unknown", },
+        { "ro.boot.lcd_density","ro.sf.lcd_density", UNSET, },
         { "ro.boot.revision",   "ro.revision",   "0", },
             // clang-format on
     };
